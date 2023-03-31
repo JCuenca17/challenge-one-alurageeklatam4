605513f920af98c1b26f0e54e682de3a1c2520f5
Jose Cuenca <jcuenca@unsa.edu.pe>
2023-03-31 03:41:54 -0500

    Reinventando card de productos

--- js/conexion/product.controller.js
+++ js/conexion/product.controller.js
@@ -2,18 +2,23 @@ import { productServices } from "./product-service.js";
 
 const createNewLine = (name, price, img, id) => {
     const line = document.createElement('div');
-    const content = `
-    <div class="product__row">
-        <img class="product__img" src="${img}" alt="producto">
+    const content = `                
+    <div class="product__card">
+        <div class="product__img">
+            <img src="${img}" alt="imagen de referencia">
+        </div>
+        <div class="product__info">
+            <h3>${name}</h3>
+            <span>$${price}</span>
+            <div class="product__description">
+                <a href="#">Conoce Más</a>
+            </div>
+        </div>
         <div class="product__text">
             <a class="product__text-edit" href="./screens/edit-product.html?id=${id}">Editar</a>
             <button class="product__text-delete" type="button" id='${id}'>Eliminar</button>
         </div>
-        <div class="product__price">
-            <h4 class="product__price-name">${name}</h4>
-            <p class="product__price-number">$${price}</p>
-        </div>
-    </div>`;
+    </div>`
     line.innerHTML = content;
     const btn = line.querySelector('button');
     btn.addEventListener('click', () => {
