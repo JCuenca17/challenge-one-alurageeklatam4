67460bb06948d9a21eecaa782f84fbad9a8513e4
Jose Cuenca <jcuenca@unsa.edu.pe>
2023-04-01 03:02:07 -0500

    Login y validación

---
+++ js/validacion/validaciones.js
@@ -0,0 +1,37 @@
+export function valida(input) {
+    const tipoDeInput = input.dataset.tipo;
+    if (input.validity.valid) {
+        input.parentElement.classList.remove('form__label-invalid');
+        input.parentElement.querySelector('.input-message-error').innerHTML = "";
+    } else {
+        input.parentElement.classList.add('form__label-invalid');
+        input.parentElement.querySelector('.input-message-error').innerHTML = mostarMensajeDeError(tipoDeInput, input);
+    }
+}
+
+const tipoDeErrores = [
+    'valueMissing',
+    'typeMismatch',
+    'patternMismatch',
+]
+
+const mensajesDeError = {
+    email: {
+        valueMissing: "Este campo no puede estar vacío",
+        typeMismatch: "El correo no es válido"
+    },
+    password: {
+        valueMissing: "Este campo no puede estar vacío",
+        patternMismatch: "La contraseña debe tener al entre 8 y 16 caracteres, al menos un dígito, al menos una minúscula y al menos una mayúscula."
+    },
+}
+
+function mostarMensajeDeError(tipoDeInput, input){
+    let mensaje = '';
+    tipoDeErrores.forEach(error => {
+        if(input.validity[error]){
+            mensaje = mensajesDeError[tipoDeInput][error];
+        }
+    });
+    return mensaje;
+}
